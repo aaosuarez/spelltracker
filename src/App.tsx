ef0bdfb76ead5fa4068367adc81e2a8435b36652
Aaron Suarez <aaosuarez@gmail.com>
2020-12-31 19:26:05 -0800

    Simple spell list

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import spells, {Spell} from './spells';
+
+const spellsByLevel: Record<number, Spell[]> = spells.reduce((result, spell) => {
+  if (result[spell.level] != null) {
+    result[spell.level].push(spell);
+  } else {
+    result[spell.level] = [spell];
+  }
+  return result;
+}, {} as Record<number, Spell[]>);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"px-4"}>
+      {Object.entries(spellsByLevel).map(([level, spells]) => {
+        return (<>
+          <div className={"uppercase text-sm sticky top-0 py-3 bg-white"}>Level {level}</div>
+          <ul className={"mb-4"}>
+            {spells.map(spell => (<li id={spell.name}>{spell.name}</li>))}
+          </ul>
+        </>)
+      })}
     </div>
   );
 }
---
+++ src/spells.ts
@@ -0,0 +1,177 @@
+export type Spell = {
+  level: number;
+  name: string;
+}
+
+const spells: Spell[] = [
+  {
+    "level": 0,
+    "name": "Arcane Mark"
+  },
+  {
+    "level": 0,
+    "name": "Bleed"
+  },
+  {
+    "level": 0,
+    "name": "Dancing Lights"
+  },
+  {
+    "level": 0,
+    "name": "Daze"
+  },
+  {
+    "level": 0,
+    "name": "Detect Magic"
+  },
+  {
+    "level": 0,
+    "name": "Detect Poison"
+  },
+  {
+    "level": 0,
+    "name": "Guidance"
+  },
+  {
+    "level": 0,
+    "name": "Light"
+  },
+  {
+    "level": 0,
+    "name": "Mending"
+  },
+  {
+    "level": 0,
+    "name": "Message"
+  },
+  {
+    "level": 0,
+    "name": "Putrefy Food and Drink"
+  },
+  {
+    "level": 0,
+    "name": "Read Magic"
+  },
+  {
+    "level": 0,
+    "name": "Resistance"
+  },
+  {
+    "level": 0,
+    "name": "Spark"
+  },
+  {
+    "level": 0,
+    "name": "Stabilize"
+  },
+  {
+    "level": 0,
+    "name": "Touch of Fatigue"
+  },
+  {
+    "level": 1,
+    "name": "Ear-Piercing Scream"
+  },
+  {
+    "level": 1,
+    "name": "Ill Omen"
+  },
+  {
+    "level": 1,
+    "name": "Mage Armor"
+  },
+  {
+    "level": 1,
+    "name": "Unseen Servant"
+  },
+  {
+    "level": 1,
+    "name": "Command"
+  },
+  {
+    "level": 1,
+    "name": "Chill Touch"
+  },
+  {
+    "level": 1,
+    "name": "Ventriloquism"
+  },
+  {
+    "level": 1,
+    "name": "Ray of Enfeeblement"
+  },
+  {
+    "level": 1,
+    "name": "Web Bolt"
+  },
+  {
+    "level": 1,
+    "name": "Burning Hands"
+  },
+  {
+    "level": 1,
+    "name": "Comprehend Languages"
+  },
+  {
+    "level": 2,
+    "name": "Blindness Deafness"
+  },
+  {
+    "level": 2,
+    "name": "Raven's Flight"
+  },
+  {
+    "level": 2,
+    "name": "Silence"
+  },
+  {
+    "level": 2,
+    "name": "Psychic Leech"
+  },
+  {
+    "level": 2,
+    "name": "Vomit Swarm"
+  },
+  {
+    "level": 2,
+    "name": "Lipstitch"
+  },
+  {
+    "level": 2,
+    "name": "Glitterdust"
+  },
+  {
+    "level": 3,
+    "name": "Howling Agony"
+  },
+  {
+    "level": 3,
+    "name": "Bestow Curse"
+  },
+  {
+    "level": 3,
+    "name": "Air Geyser"
+  },
+  {
+    "level": 3,
+    "name": "Harrowing"
+  },
+  {
+    "level": 3,
+    "name": "Haste"
+  },
+  {
+    "level": 3,
+    "name": "Dispel Magic"
+  },
+  {
+    "level": 4,
+    "name": "Black Tentacles"
+  },
+  {
+    "level": 4,
+    "name": "Divination"
+  }
+];
+
+export default spells;
