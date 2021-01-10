43c8a3126b3fdc9496bb64bd731fcb8c5dbba434
Aaron <aaosuarez@gmail.com>
2021-01-09 20:42:55 -0800

    Extract types

--- src/types.ts
+++ src/types.ts
@@ -6,14 +6,17 @@ export enum Mode {
 export type Hex = {
   name: string;
 };
+
 export type Spell = {
   level: number;
   name: string;
   id: number;
 };
+
 export type Scroll = {
   name: string;
 };
+
 export type Wand = {
   id: number;
   name: string;
